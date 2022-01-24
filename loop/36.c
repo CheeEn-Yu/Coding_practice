c7f24ae898acb37984bb46ffed0cd1118cc3468a
CheeEn-Yu <allenyu172@gmail.com>
2022-01-24 21:01:53 +0800

    .

--- loop/36.c
+++ loop/36.c
@@ -3,6 +3,9 @@ int main(){
     int year, month, first_day;
     scanf("%d%d%d", &year,&month,&first_day);
     printf(" Su Mo Tu We Th Fr Sa\n");
+    printf("=====================\n");
+    
+    printf("=====================");
     
 
     return 0;
