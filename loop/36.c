0cfc7c873ef1ddab5321d996fb9770eb2f19efae
CheeEn-Yu <allenyu172@gmail.com>
2022-01-26 13:27:51 +0800

    v1

--- loop/36.c
+++ loop/36.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 int main(){
-    int year, month[13] = {0,1,2,3,4,5,6,7,8,9,11,12}, date[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31}, first_day;
+    int year, month, date[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31}, first_day;
     scanf("%d%d%d", &year,&month,&first_day);
-    printf(" Su Mo Tu We Th Fr Sa\n");
-    printf("=====================\n");
     if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) && month == 2)
         date[2]++;
-    for (int i = 0; i < date[month]; i++){
-        
+    if (month > 13 || month < 1 || first_day > 7 || first_day < 0)
+        printf("invalid");
+    else{
+        int width = 3 * first_day;
+        printf(" Su Mo Tu We Th Fr Sa\n");
+        printf("=====================\n");
+        for (int i = 0; i < first_day; i++)
+            printf("   ");
+        for (int i = 1; i <= date[month]; i++){
+            printf("%3d", i);
+            width+=3;
+            if (width == 21){
+                width = 0;
+                printf("\n");
+            }
+        }
+        printf("\n=====================");
     }
-
-    printf("=====================");
-    
 
     return 0;
 }
