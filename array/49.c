2f9d029d1757fa0699fcd67204aabe083b85fe4c
CheeEn-Yu <allenyu172@gmail.com>
2022-02-14 11:15:46 +0800

    yes

---
+++ array/49.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int N,m;
+    scanf("%d%d",&N,&m);
+    int data[N];
+    for (int i = 0; i < N; i++)
+        scanf("%d",&data[i]);
+    int max = 0;
+    for(int i = 0; i < N; i++){
+        
+    }
+
+    return 0;
+}
