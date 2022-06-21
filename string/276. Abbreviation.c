2e7853d6dcbec07a5a5f12dbfefa270092dd859e
Win10 <allenyu172@gmail.com>
2022-06-21 21:51:02 +0800

    6/21

--- string/276. Abbreviation.c
+++ string/276. Abbreviation.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 int main(){
-    
+    char in[127];
+    while (scanf("%s", in)!=EOF){
+        if (strcmp(in, "the") != 0 && strcmp(in, "of") != 0 && strcmp(in,"and") && strcmp(in, "at") &&
+        strcmp(in, "the.") != 0 && strcmp(in, "of.") != 0 && strcmp(in,"and.") && strcmp(in, "at."))
+            printf("%c", toupper(in[0]));
+        if (in[strlen(in) - 1] == '.') printf("\n");
+
+    }
     return 0;
 }
