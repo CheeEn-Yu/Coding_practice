b28a89dcfe3e5bfcc0b23e8769453bfd98680f3b
CheeEn-Yu <allenyu172@gmail.com>
2022-06-25 00:28:03 +0800

    6/25

---
+++ string/46. Play with Words.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char first_command[6];
+    char second_command[5];
+    while (scanf("%s", first_command) != EOF){
+        
+    }
+
+
+    return 0;
+}
