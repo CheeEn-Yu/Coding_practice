cf4c50b70d8ddacf95caa60c9beb28eac66aadf5
Win10 <allenyu172@gmail.com>
2022-09-21 00:33:16 +0800

    20220927

---
+++ Bit_Operation/281.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int data;
+    while (scanf("%d", &data) != EOF){
+        int count = 0;
+        while(data!=0){
+            if(data%2) {
+                count++;
+            }
+            data/=2;
+        }
+        printf("%d\n", count);
+    }
+    
+    return 0;
+}
